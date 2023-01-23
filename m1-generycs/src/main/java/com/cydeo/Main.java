80e00ec819a4c91b3f0f2b4ead6e9cec30df9fc5
owner <mahirsarac89@gmail.com>
2023-01-23 08:38:03 -0500

    after geneyrics Arraylist works

--- m1-generycs/src/main/java/com/cydeo/Main.java
+++ m1-generycs/src/main/java/com/cydeo/Main.java
@@ -10,7 +10,7 @@ public class Main {
         items.add(2);
         items.add(3);
         items.add(4);
-        items.add("Apple");
+       // items.add("Apple");
         printdoubled(items);
     }
 
@@ -19,5 +19,15 @@ public class Main {
 
             System.out.println((Integer)i*2);
         }
+        ArrayList<Integer>items1=new ArrayList<>();
+        //ArrayList only accepts Integer data type
+        items1.add(1);
+        items1.add(2);
+        items1.add(3);
+        items1.add(4);
+        for (Integer i : items1) {
+            System.out.println(i*2);
+        }
+
     }
 }
