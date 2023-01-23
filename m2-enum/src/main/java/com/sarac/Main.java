66e962e1d640c8188b83ebae0abef7c967ff6028
owner <mahirsarac89@gmail.com>
2023-01-23 10:45:37 -0500

    calclator created by using enum

--- m2-enum/src/main/java/com/sarac/Main.java
+++ m2-enum/src/main/java/com/sarac/Main.java
@@ -36,5 +36,31 @@ public class Main {
 
         }
 
+        System.out.println("Operation task");
+        calculator(25.0,25.0,mathOperators.SUM);
+        calculator(25,25,mathOperators.MULTIPLLICATION);
+        calculator(27,15,mathOperators.SUBTRACTION);
+        calculator(26,13,mathOperators.DIVISION);
+
+
+    }
+
+    private static void calculator(double num1, double num2,mathOperators operators) {
+        switch (operators){
+            case SUM:
+                System.out.println(num1 + num2);
+                break;
+            case SUBTRACTION:
+                System.out.println(num1 - num2);
+                break;
+            case MULTIPLLICATION:
+                System.out.println(num1 * num2);
+                break;
+            case DIVISION:
+                System.out.println(num1 / num2);
+                break;
+        }
+
+
     }
 }
