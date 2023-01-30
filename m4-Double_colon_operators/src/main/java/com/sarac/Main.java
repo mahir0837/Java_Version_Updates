530241680af757ef360e884f266074465694fe39
owner <mahirsarac89@gmail.com>
2023-01-29 20:41:36 -0500

    m4 double colon operators created

---
+++ m4-Double_colon_operators/src/main/java/com/sarac/Main.java
@@ -0,0 +1,11 @@
+package com.sarac;
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Predicate<Integer> pred= p->p%2==0?true:false;
+
+    }
+}
