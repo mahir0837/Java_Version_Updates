32e5c1a70161e96650d85187959d61f39d7a406a
owner <mahirsarac89@gmail.com>
2023-01-30 12:55:13 -0500

    AppleTest Class updated

--- m5-comparator_interface/src/main/java/com/sarac/AppleTest.java
+++ m5-comparator_interface/src/main/java/com/sarac/AppleTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+
 public class AppleTest {
 
     public static void main(String[] args) {
@@ -14,11 +16,15 @@ public class AppleTest {
         inventory.add(new Apple(200,Color.GREEN));
         inventory.add(new Apple(50,Color.RED));
 
-        Comparator<Apple>sortApple=Comparator.comparing((Apple apple)->apple.getWeight());
+        Comparator<Apple>sortApple=comparing((Apple apple)->apple.getWeight());
         inventory.sort(sortApple);
         System.out.println(inventory);
         System.out.println("*********************************");
-        inventory.sort(Comparator.comparing(Apple::getWeight));
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+        System.out.println("********************************");
+        inventory.sort(comparing(Apple::getWeight).reversed());
         System.out.println(inventory);
 
 
