9de0795de370be15128bfc9dd122ccd0b391ea80
owner <mahirsarac89@gmail.com>
2023-02-06 13:32:26 -0500

    FactoryMethod

---
+++ m8-new-version-updates/src/main/java/com/sarac/java9/FactoryMethodDemo.java
@@ -0,0 +1,33 @@
+package com.sarac.java9;
+
+import java.util.*;
+
+public class FactoryMethodDemo {
+
+    public static void main(String[] args) {
+
+        //Creating unmodifiable List Before Java9
+
+        List<String> courses= Collections.unmodifiableList(
+                Arrays.asList("Java","Spring","Agile")
+        );
+        courses.add("TS");
+        System.out.println(courses);
+        //Creating unmodifiable List After Java9
+
+        List<String> myCourses= List.of("Java","Spring","Agile");
+
+        //Creating unmodifiable Set After Java9
+
+        Set<String>myProducts=Set.of("Milk","Bread","Butter");
+
+        //Creating unmodifiable Map After Java9
+
+        Map<String,Integer>myCart=Map.ofEntries(
+
+                Map.entry("Samsung TV",1),
+                Map.entry("PSP",5),
+                Map.entry("Cahir",5)
+        );
+    }
+}
